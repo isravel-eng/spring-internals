a557c0468348d252b3ab28eb3a7cd69435a267f9
ISRAVEL Y <isravelyosva@gmail.com>
2026-07-12 10:59:27 +0530

    To see working of auto configuration

---
+++ 01-spring-boot/04-auto-configuration/Code/AutoConfigurationWorking/src/main/java/com/sfl/autoconfigurationworking/AutoConfigurationWorkingApplication.java
@@ -0,0 +1,13 @@
+package com.sfl.autoconfigurationworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConfigurationWorkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigurationWorkingApplication.class, args);
+    }
+
+}
